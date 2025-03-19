69c7209216d54ea96bfd5a5ff5006bec36ed5b69
nithishkumar2022020 <reddynithish15@gmail.com>
2025-03-20 00:44:39 +0530

    Add JavaScript Netlify Function

---
+++ backend/netlify/functions/api.js
@@ -0,0 +1,14 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    },
+    body: JSON.stringify({
+      message: 'Hello from the API!'
+    })
+  };
+}; 
